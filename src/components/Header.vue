c8a1913e995c6ee8a180a83651a989969c1254c1
mehrnoosh <mehr@gmail.com>
2023-06-07 20:25:05 +0330

    first commit

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,14 +9,14 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-            <router-link class="nav-link active" to="/" aria-current="page" >Home</router-link>     
+            <router-link class="nav-link active" :to="{'name':'home'}" aria-current="page" >Home</router-link>     
         </li>
         <li class="nav-item">
-            <router-link class="nav-link" to="/users">Users</router-link>
+            <router-link class="nav-link" :to="{'name':'users'}">Users</router-link>
         </li>
         
         <li class="nav-item">
-            <router-link class="nav-link" to="/posts">Posts</router-link>          
+            <router-link class="nav-link" :to="{'name':'posts'}">Posts</router-link>          
         </li>
       </ul>
      
